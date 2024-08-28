b059bc9e9f3e2c1088df041e050d908927b6ee61
Matheus Martins Guimarães <ma17martins@gmail.com>
2024-08-28 19:16:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Project-fetch-github-api
+
+## Description
+This is a project proposed by the DevQuest Course, which consists of a JavaScript module challenge. The goal is to create a search field where a user's name must be entered to access their profile. By clicking the "Search" button or pressing the "Enter" key, the system performs a query through the GitHub API, displaying the user's name, photo, bio description, and repositories.
+
+## 💻 Overview of
+
+[<img src="./src/image/screen-project-fetch-github-api.gif" alt="">](https://github.com/ma17martins/project-fetch-github-api.git)
+
+## 📳 Responsive
+
+[<img src="./src/image/screen-project-fetch-github-api-mobile.gif">](https://github.com/ma17martins/project-fetch-github-api.git)
+## Technologies used
+
+- HTML 
+- CSS 
+- JS
+- API
+
+
+ # Social media
+
+ - 👋🏾 [Linkedin](https://www.linkedin.com/in/matheus17martins/)
+ - 💻 [Github](https://github.com/ma17martins)
+
---
+++ src/scripts/index.js
@@ -0,0 +1,52 @@
+import { getUser } from './services/user.js'
+import { getRepositories } from './services/repositories.js'
+import { user } from './objects/user.js'
+import { screen } from './objects/screen.js'
+
+
+
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value
+    if (validateEmptyInput(userName)) return
+    getUserData(userName)
+})
+
+document.getElementById('input-search').addEventListener('keyup', (e) => {
+    const userName = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterKeyPressed = key === 13
+
+    if (isEnterKeyPressed) {
+        if (validateEmptyInput(userName)) return
+        getUserData(userName)
+    }
+})
+
+function validateEmptyInput(userName) {
+    if (userName.length === 0) {
+        alert('Fill in the field with the GitHub username.')
+        return true
+    }
+}
+
+
+
+async function getUserData(userName) {
+
+    const userResponse = await getUser(userName)
+    console.log(userResponse);
+
+    if (userResponse.message === "Not Found") {
+        console.log('caiu aqui');
+        screen.renderNotFound()
+        return
+    }
+
+    const repositoriesResponse = await getRepositories(userName)
+
+    user.setInfo(userResponse)
+
+    user.setRepositories(repositoriesResponse)
+
+    screen.renderUser(user)
+}
